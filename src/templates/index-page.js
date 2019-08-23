d9a1ba8f79a697eb448254cec0e540c2593a0081
CJ Kenney <cjkenney.dev@gmail.com>
2019-08-22 21:03:36 -0500

    remove index page blurbs

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
-import Features from '../components/Features';
 import BlogRoll from '../components/BlogRoll';
 
 export const IndexPageTemplate = ({
@@ -84,7 +83,6 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <Features gridItems={intro.blurbs} />
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
