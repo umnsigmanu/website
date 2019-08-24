a84b9c3ef45a4255b8264a88abd8a0b4afb4d282
CJ Kenney <cjkenney.dev@gmail.com>
2019-08-24 18:57:30 -0500

    add 'about our chapter' link

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -77,7 +77,7 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      About our chapter
                     </Link>
                   </div>
                 </div>
