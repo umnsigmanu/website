9d0bf9fdd66f1ef28e14a488953e442b6d1c128d
CJ Kenney <cjkenney.dev@gmail.com>
2019-08-26 18:20:44 -0500

    fix blurbs build error

--- src/templates/alumni-page.js
+++ src/templates/alumni-page.js
@@ -122,16 +122,6 @@ export const alumniPageQuery = graphql`
         heading
         description
         intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
           heading
           description
         }
