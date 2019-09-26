5dc2a1b32372f523313459617fea8aae0fbe676a
CJ Kenney <cjkenney.dev@gmail.com>
2019-09-25 20:14:47 -0500

    add donor benefit table

--- src/templates/alumni-page.js
+++ src/templates/alumni-page.js
@@ -40,21 +40,75 @@ export const AlumniPageTemplate = ({
             <div className="column is-10 is-offset-1">
               <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
               <p>{description}</p>
-              <h3 className="has-text-weight-semibold is-size-3">
+              <h3 style={{textAlign: 'center'}} className="has-text-weight-semibold is-size-3">
                 {main.heading}
               </h3>
               <p>{main.description}</p>
-              <h3 className="has-text-weight-semibold is-size-3">
+              <h3 style={{textAlign: 'center'}} className="has-text-weight-semibold is-size-3">
                 {donation.heading}
               </h3>
               <p>{donation.description}</p>
               {/* Paypal Donation Link */}
-              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <form 
+                action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"
+                style={{textAlign: 'center'}}
+              >
                 <input type="hidden" name="cmd" value="_s-xclick" />
                 <input type="hidden" name="hosted_button_id" value="M3YW2QZ43WG54" />
                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
               </form>
+              <h3 style={{textAlign: 'center'}} className="has-text-weight-semibold is-size-3">Donor Benefits</h3>
+              <table style={{margin: '0 auto', border: '1px solid black'}}>
+                <tr>
+                  <th></th>
+                  <th>Donor Any Amount</th>
+                  <th>Contributor $500+</th>
+                  <th>Benefactor $1,000+</th>
+                  <th>Sponsor $5,000+</th>
+                  <th>Patron $10,000+</th>
+                </tr>
+                <tr>
+                  <td>Recognition on our website</td>
+                  <td>x</td>
+                  <td>x</td>
+                  <td>x</td>
+                  <td>x</td>
+                  <td>x</td>
+                </tr>
+                <tr>
+                  <td>Recognition on a plaque in the Arthur A. Barlow Library</td>
+                  <td></td>
+                  <td>x</td>
+                  <td>x</td>
+                  <td>x</td>
+                  <td>x</td>
+                </tr>
+                <tr>
+                  <td>Honorary brick installed on the porch</td>
+                  <td></td>
+                  <td></td>
+                  <td>x</td>
+                  <td>x</td>
+                  <td>x</td>
+                </tr>
+                <tr>
+                  <td>Cookout held at the house for the donor</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>x</td>
+                  <td>x</td>
+                </tr>
+                <tr>
+                  <td>Name a room in the chapter house</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>x</td>
+                </tr>
+              </table>
               <div
                 className="full-width-image-container"
                 style={{
