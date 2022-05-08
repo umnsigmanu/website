b2776a553fae15e9dde37c3cf895b5c05b366145
umnsigmanu <umnsigmanu@gmail.com>
2022-05-08 12:43:26 -0500

    remove blog roll

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
-import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
@@ -28,7 +27,7 @@ export default class BlogIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <BlogRoll />
+              {/* <BlogRoll /> */}
             </div>
           </div>
         </section>
