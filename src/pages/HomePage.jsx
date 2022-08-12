01722f0b8bb9cfd506209d32dd6ae1a92c430146
umnsigmanu <umnsigmanu@gmail.com>
2022-08-11 22:56:12 -0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { NavigationBar } from '../components/NavigationBar';
 import { Footer } from '../components/Footer';
 import { BannerImage } from '../components/BannerImage';
-import { ContentContainer, Button } from '../styles/globalStyles.styled';
+import { CenterContainer, ContentContainer, Button } from '../styles/globalStyles.styled';
 import AboutBanner from '../images/banner-about.jpg';
 
 export const HomePage = () => {
@@ -15,10 +15,17 @@ export const HomePage = () => {
       />
       <ContentContainer>
         <h1>Sigma Nu Fraternity</h1>
-        <p>The Gamma Tau chapter of Sigma Nu Fraternity was founded at the University of Minnesota in 1904. Since our founding, our brothers have built a tradition of excellence and have dedicated their lives to our Fraternity's values of Love, Honor, and Truth.</p>
-        <Button to="about">About Our Chapter</Button>
+        <p>
+          The Gamma Tau chapter of Sigma Nu Fraternity was founded at the
+          University of Minnesota in 1904. Since our founding, our brothers have
+          built a tradition of excellence and have dedicated their lives to our
+          Fraternity's values of Love, Honor, and Truth.
+        </p>
+        <CenterContainer>
+          <Button to='about'>About Our Chapter</Button>
+        </CenterContainer>
       </ContentContainer>
       <Footer />
     </>
-  )
-}
+  );
+};
